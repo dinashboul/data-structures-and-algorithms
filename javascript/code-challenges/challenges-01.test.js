2969b96f446a9656a58753e110c47110f2283040
dinashboul <dinaalshboul@gmail.com>
2022-08-21 22:47:43 +0300

    new

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -10,6 +10,7 @@ Use `forEach` to loop over the input array and work with each value.  Push the n
 
 const addOne = (arr) => {
   // Solution code here...
+  //decleare new array
   const newArr=[];
   arr.forEach(item => {
     newArr.push(item+1);
@@ -60,9 +61,9 @@ CHALLENGE 4
 
 Write a function named `greeting` that takes in a single string and returns the string in all uppercase letters, and followed by an "!".
 
-Then, write a function named `speaker` that takes in an array of strings and a callback function. 
+Then, write a function named `speaker` that takes in an array of strings and a callback function.
 
-Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array. 
+Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array.
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
@@ -75,7 +76,7 @@ const speaker = (words, callback) => {
   const newArr=[];
   words.forEach(item => {
     let st=callback(item);
-    newArr.push(st)   
+    newArr.push(st);
   });
   return newArr;
 };
